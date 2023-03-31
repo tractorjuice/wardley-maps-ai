51c9a38230d2e5f50f0a2e30bf36cab5c447e851
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-03-31 17:32:35 +0100

    Update wardley_maps_ai.py

--- wardley_maps_ai.py
+++ wardley_maps_ai.py
@@ -1,38 +1,30 @@
 import streamlit as st
 import requests
 
-st.set_page_config(page_title="Wardley Map Viewer")
+# Define the API URL
+API_URL = "https://api.onlinewardleymaps.com/v1/maps/fetch"
 
-# Define the base URL for the API
-BASE_URL = "https://api.onlinewardleymaps.com/v1/maps/fetch?id="
-
-def fetch_wardley_map(map_id):
-    """
-    Fetches the wardley map with the given ID from the API.
-    """
-    url = BASE_URL + map_id
-    response = requests.get(url)
+def get_wardley_map(map_id):
+    # Call the API with the provided map_id
+    response = requests.get(f"{API_URL}?id={map_id}")
+    # Check the status code of the response
     if response.status_code == 200:
+        # Return the JSON response
         return response.json()
     else:
-        return None
+        # Raise an exception if there was an error
+        raise Exception(f"Error fetching Wardley Map with ID {map_id}. Status code: {response.status_code}")
 
-# Create the sidebar
-st.sidebar.title("Wardley Map Viewer")
-map_id = st.sidebar.text_input("Enter the ID of the wardley map:")
+# Create a Streamlit app
+def app():
+    # Set the app title
+    st.set_page_config(page_title="Wardley Maps Viewer")
 
-# Fetch the map data and display it if available
-if map_id:
-    map_data = fetch_wardley_map(map_id)
-    if map_data:
-        st.write("## Wardley Map")
-        st.image(map_data["image_url"])
-    else:
-        st.write("Invalid map ID")
-
-# Create a box to ask questions about the wardley map
-st.write("## Ask a Question")
-question = st.text_input("Enter your question here:")
-if question:
-    st.write(f"You asked: {question}")
-    # Code to process the question and provide an answer goes here
+    # Add a form to input the map ID
+    map_id = st.text_input("Enter the ID of the Wardley Map you want to view:")
+    # Add a button to submit the form
+    if st.button("Load Map"):
+        # Call the get_wardley_map function to fetch the map data
+        map_data = get_wardley_map(map_id)
+        # Display the map using an iframe
+        st.components.v1.iframe(src=f"https://onlinewardleymaps.com/maps/{map_id}", height=600, scrolling=True)
