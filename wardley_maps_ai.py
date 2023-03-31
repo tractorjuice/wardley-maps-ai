9e7501902c5a4337a9ed01baa42b7b680d26ca8d
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-03-31 19:50:00 +0100

    Update wardley_maps_ai.py

--- wardley_maps_ai.py
+++ wardley_maps_ai.py
@@ -64,8 +64,6 @@ def app():
             prompt_wardley_ai = prompt.format(question=question, map=map_data)
             response = llm(prompt_wardley_ai)
             st.write(response)
-            
-            st.write(map_data)
                    
         else:
             st.error("Map not found. Please enter a valid ID.")
