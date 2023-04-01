27d6ad8137fb9637d7ec968cf8e94202d46ff583
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-01 13:44:00 +0100

    Update wardley_maps_ai.py

--- wardley_maps_ai.py
+++ wardley_maps_ai.py
@@ -18,102 +18,26 @@ from langchain.schema import (
 API_ENDPOINT = "https://api.onlinewardleymaps.com/v1/maps/fetch?id="
 
 template = """
-    Assistant is a large language model trained by OpenAI.
-    Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-    Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-    Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-    
-    Your goal is to provide assistance on wardley maps and always give a verbose answer. The following explains how the wardley map is formatted:
+Your goal is to provide assistance on wardley maps and always give a verbose answer. The following explains how the wardley map is formatted:
 
-Wardley Map
-Usage
-To set the title
-Example: title My Wardley Map
-To create a component
-component Name [Visibility (Y Axis), Maturity (X Axis)]
-Example: component Customer [0.9, 0.5]
-component Cup of Tea [0.9, 0.5]
-To create a market
-market Name [Visibility (Y Axis), Maturity (X Axis)]
-Example: market Customer [0.9, 0.5]
-market Cup of Tea [0.9, 0.5]
-evolve Customer 0.9 (market)
-Inertia - component likely to face resistance to change.
-component Name [Visibility (Y Axis), Maturity (X Axis)] inertia
-Example: component Customer [0.95, 0.5] inertia
-component Cup of Tea [0.9, 0.5] inertia
-market Cup of Tea [0.9, 0.5] inertia
-To evolve a component
-evolve Name (X Axis)
-Example: evolve Customer 0.8
-evolve Cup of Tea evolve 0.8
-To link components
-Example:
+Thank you for providing the detailed explanation of the Wardley Map formatting. Here is a summary of the elements in the format:
 
-Start Component->End Component
-Customer->Cup of Tea
-To indicate flow
-Example: Start Component+<>End Component
-Customer+<>Cup of Tea
-To set component as pipeline:
-pipeline Component Name [X Axis (start), X Axis (end)]
-Example: pipeline Customer [0.15, 0.9]
-pipeline Customer
-To indicate flow - past components only
-Example: Start Component+<End Component
-Hot Water+<Kettle
-To indicate flow - future components only
-Example: Start Component+>End Component
-Hot Water+>Kettle
-To indicate flow - with label
-Example: Start Component+'insert text'>End Component
-Hot Water+'$0.10'>Kettle
-Pioneers, Settlers, Townplanners area
-Add areas indicating which type of working approach supports component development Example:
-
-pioneers [<visibility>, <maturity>, <visibility2>, <maturity2>]
-settlers [0.59, 0.43, 0.49, 0.63]
-townplanners [0.31, 0.74, 0.15, 0.95]
-Build, buy, outsource components
-Highlight a component with a build, buy, or outsource method of execution Example:
-
-build <component>
-buy <component>
-outsource <component>
-component Customer [0.9, 0.2] (buy)
-component Customer [0.9, 0.2] (build)
-component Customer [0.9, 0.2] (outsource)
-evolve Customer 0.9 (outsource)
-evolve Customer 0.9 (buy)
-evolve Customer 0.9 (build)
-Link submap to a component
-Add a reference link to a submap. A component becomes a link to an other Wardley Map Example:
-
-submap Component [<visibility>, <maturity>] url(urlName)
-url urlName [URL]
-submap Website [0.83, 0.50] url(submapUrl)
-url submapUrl [https://onlinewardleymaps.com/#clone:qu4VDDQryoZEnuw0ZZ]
-Stages of Evolution
-Change the stages of evolution labels on the map Example:
-
-evolution First->Second->Third->Fourth
-evolution Novel->Emerging->Good->Best
-Y-Axis Labels
-Change the text of the y-axis labels Example:
-
-y-axis Label->Min->Max
-y-axis Value Chain->Invisible->Visible
-Add notes
-Add text to any part of the map Example:
-
-note Note Text [0.9, 0.5]
-note +future development [0.9, 0.5]
-Available styles
-Change the look and feel of a map Example:
-
-style wardley
-style handwritten
-style colour
+Title: Set the title with title.
+Components: Create a component with component Name [Visibility, Maturity].
+Market: Create a market with market Name [Visibility, Maturity].
+Inertia: Indicate resistance to change with inertia.
+Evolve: Evolve a component with evolve Name (X Axis).
+Links: Link components with Start Component->End Component.
+Flow: Indicate flow with various options such as Start Component+<>End Component.
+Pipeline: Set a component as a pipeline with pipeline Component Name [X Axis (start), X Axis (end)].
+Pioneers, Settlers, Townplanners area: Add areas to indicate the working approach with pioneers, settlers, and townplanners.
+Build, buy, outsource: Indicate the method of execution with build, buy, or outsource.
+Submap: Link a submap to a component with submap Component [visibility, maturity] url(urlName) and url urlName [URL].
+Stages of Evolution: Customize the stages of evolution labels with evolution.
+Y-Axis Labels: Customize the Y-axis labels with y-axis.
+Notes: Add notes to the map with note.
+Styles: Change the look and feel of the map with style.
+This formatting makes it easy to create and modify Wardley Maps, and it's helpful for understanding the structure and connections between components.
 
 WARDLEY MAP: {map}
 QUESTION: {question}
