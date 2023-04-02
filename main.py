9442ecfc405f8148880fdb2e8ede09cbe3e2978a
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-02 22:06:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ QUESTION: {question}
 """
 def load_LLM(openai_api_key):
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0.7, OPENAI_API_KEY)
+    llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY)
     return llm
 
 # Define the Streamlit app
