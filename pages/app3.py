f8c26fea3b4cd43ebe082c3168f85dc0b23cb4da
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-03 20:26:32 +0100

    Update app3.py

--- pages/app3.py
+++ pages/app3.py
@@ -9,6 +9,11 @@ from langchain.prompts.chat import (
     AIMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
+import llama_index
+from llama_index import LLMPredictor, GPTSimpleVectorIndex, PromptHelper
+import os
+from pathlib import Path
+from gpt_index import download_loader
 from langchain.schema import (
     AIMessage,
     HumanMessage,
@@ -17,6 +22,13 @@ from langchain.schema import (
 
 API_ENDPOINT = "https://api.onlinewardleymaps.com/v1/maps/fetch?id="
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+
+# Set the page title and layout
+st.set_page_config(page_title="Chat with your map")
+st.title("Chat with your map")
+st.sidebar.markdown("# Ask Questions about Your Map")
+st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
+st.sidebar.markdown("Current Version: 0.0.2")
 
 template = """
 Your goal is to provide assistance on wardley maps and always give a verbose answer. The following explains how the wardley map is formatted:
@@ -62,62 +74,83 @@ def load_LLM(openai_api_key):
     llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY, max_tokens=500)
     return llm
 
-# Define the Streamlit app
-def app():
+def show_messages(text):
+    messages_str = [
+        f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
+    ]
+    text.text_area("Messages", value=str("\n".join(messages_str)), height=300)
 
-    # Set the page title and layout
-    st.set_page_config(page_title="Chat with your map")
-    st.title("Chat with your map")
-    st.sidebar.markdown("# Ask Questions about Your Map")
-    
-    #st.sidebar.image("images/shakespeare.png")
-    st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
-    st.sidebar.markdown("Current Version: 0.0.2")
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
+
+# Define the form to enter the map ID
+map_id = st.text_input("Enter the ID of the Wardley Map: For example https://onlinewardleymaps.com/#clone:OXeRWhqHSLDXfOnrfI, enter: OXeRWhqHSLDXfOnrfI", value="OXeRWhqHSLDXfOnrfI")
+question = st.text_input(label="Question ", value="Create a numbered list of the components in this wardley map.", key="q_input", max_chars=150)
+if len(question.split(" ")) > 700:
+	st.write("Please enter a shorter question about your Wardley Map")
+	st.stop()
+
+# Load the map data when the user submits the form
+if st.button("Ask Question to Wardley AI"):
+	# Fetch the map data from the API
+    url = f"https://api.onlinewardleymaps.com/v1/maps/fetch?id={map_id}"
+    response = requests.get(url)
+
+	# Check if the map was found
+	if response.status_code == 200:
+		map_data = response.json()
+
+        # Display the map
+        #st.write(map_data)
+            
+        prompt = PromptTemplate(
+			input_variables=["map", "question"],
+			template=template,
+		)
+		
+		#st.markdown("### Prompt:")
+		#st.write(prompt)
+		
+		llm = load_LLM(OPENAI_API_KEY)
+		prompt_wardley_ai = prompt.format(question=question, map=map_data)
+		response = llm(prompt_wardley_ai)
+		
+		#st.markdown("### Input Prompt:")
+		#st.write(prompt_wardley_ai)
+		
+		#st.markdown("### Question:")
+		#st.write(question)
+		
+		st.markdown("### Response:")
+		st.write(response)
+	else:
+        st.error("Map not found. Please enter a valid ID.")
+
+if "messages" not in st.session_state:
+	st.session_state["messages"] = BASE_PROMPT
+
+text = st.empty()
+show_messages(text)
+
+prompt = st.text_input("Prompt", value="What is this Wardley Map about?")
+
+if st.button("Send"):
+    with st.spinner("Generating response..."):
+
+        # st.session_state["messages"] += [{"role": "user", "content": prompt}]
+        # response = openai.ChatCompletion.create(
+        #    model="gpt-3.5-turbo", messages=st.session_state["messages"]
+        #)
         
-    # Define the form to enter the map ID
-    map_id = st.text_input("Enter the ID of the Wardley Map: For example https://onlinewardleymaps.com/#clone:OXeRWhqHSLDXfOnrfI, enter: OXeRWhqHSLDXfOnrfI", value="OXeRWhqHSLDXfOnrfI")
-    question = st.text_input(label="Question ", value="Create a numbered list of the components in this wardley map.", key="q_input", max_chars=150)
-    if len(question.split(" ")) > 700:
-        st.write("Please enter a shorter question about your Wardley Map")
-        st.stop()
+        response = index.query(prompt)
+        
+        # message_response = response["choices"][0]["message"]["content"]
+        # st.session_state["messages"] += [
+        #    {"role": "system", "content": message_response}
+        #]
+        # show_messages(text)
+        text.text_area("Messages", response, height=300)
 
-    # Load the map data when the user submits the form
-    if st.button("Ask Question to Wardley AI"):
-        # Fetch the map data from the API
-        url = f"https://api.onlinewardleymaps.com/v1/maps/fetch?id={map_id}"
-        response = requests.get(url)
-
-        # Check if the map was found
-        if response.status_code == 200:
-            map_data = response.json()
-
-            # Display the map
-            #st.write(map_data)
-            
-            prompt = PromptTemplate(
-                input_variables=["map", "question"],
-                template=template,
-            )
-            
-            #st.markdown("### Prompt:")
-            #st.write(prompt)
-                                    
-            llm = load_LLM(OPENAI_API_KEY)
-            
-            prompt_wardley_ai = prompt.format(question=question, map=map_data)
-            response = llm(prompt_wardley_ai)
-            
-            #st.markdown("### Input Prompt:")
-            #st.write(prompt_wardley_ai)
-            
-            #st.markdown("### Question:")
-            #st.write(question)
-            
-            st.markdown("### Response:")
-            st.write(response)
-                   
-        else:
-            st.error("Map not found. Please enter a valid ID.")
-                                                                                                                          
-if __name__ == "__main__":
-    app()
+if st.button("Clear"):
+    st.session_state["messages"] = BASE_PROMPT
+    show_messages(text)
