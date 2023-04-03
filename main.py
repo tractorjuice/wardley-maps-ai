c78ee94bdf913870917fa691bf085d0a7ef83831
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-03 19:29:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
     **Select a demo from the sidebar** to see some examples
     of what Streamlit can do!
     ### Want to learn more?
