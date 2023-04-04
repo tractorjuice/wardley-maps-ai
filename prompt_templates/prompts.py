101df5a013759edcebee16290a34b0c769245cba
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-04 19:39:56 +0100

    prompt updates

---
+++ prompt_templates/prompts.py
@@ -0,0 +1,58 @@
+# Test prompt templates and chaining
+import langchain
+import prompts
+
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+template1 = """
+I want you to act as a naming consultant for new companies.
+
+Here are some examples of good company names:
+
+- search engine, Google
+- social media, Facebook
+- video sharing, YouTube
+
+The name should be short, catchy and easy to remember.
+
+What is a good name for a company that makes {product}?
+"""
+
+prompt = PromptTemplate(
+    input_variables=["Wardley Maps"],
+    template=template,
+)
+
+template2 = "You are a helpful assistant that translates {input_language} to {output_language}."
+
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_template="{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+# get a chat completion from the formatted messages
+chat_prompt.format_prompt(input_language="English", output_language="French", text="I love programming.").to_messages()
+
+prompt=PromptTemplate(
+    template="You are a helpful assistant that translates {input_language} to {output_language}.",
+    input_variables=["input_language", "output_language"],
+)
+system_message_prompt = SystemMessagePromptTemplate(prompt=prompt)
+
+f = open('../wardley_maps/prompt_engineering_wardley_map.txt', 'r')
+wmmap = f.read()
+f.close()
+
+prompt = load_prompt("wmprompt_1.json")
+print(prompt.format(adjective="Prompt Engineering", content="wmmap"))
