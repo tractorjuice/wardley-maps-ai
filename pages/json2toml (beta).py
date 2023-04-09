752512411220df6e66597ec018284565ff8f91ce
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-09 10:37:15 +0100

    Update json2toml (beta).py

--- pages/json2toml (beta).py
+++ pages/json2toml (beta).py
@@ -63,10 +63,52 @@ if selected == "JSON to TOML":
         st.download_button(
             "DOWNLOAD TOML FILE", data=toml_content, file_name=toml_file_name
         )
-elif selected == "Page 2":
-    st.title("Page 2")
+        
+elif selected == "WM to TOML":
+    st.title("WM to TOML")
+    
+    st.image("./pages/logo.gif", width=200)
+    st.title("WM to TOML File Converter")
+    st.write(
+        """  
+            """
+    )
+
+    st.write(
+        """  
+    Let's convert your Wardley Map in WM to TOML
+            """
+    )
+
+    st.write(
+        """  
+            """
+    )
+    json_file = st.file_uploader("UPLOAD JSON FILE")
+    st.info(
+        f"""
+                👆 Upload your json file. Or try a [sample](https://github.com/CharlyWargnier/CSVs/blob/master/more_samples/firestore-key-sample.json?raw=true).
+                
+                """
+    )
+
+    if json_file is not None:
+        json_text = json_file.read()
+
+        st.write("JSON CONTENT")
+        st.code(json.loads(json_text))
+
+        toml_content = toml.dumps(json.loads(json_text))
+        st.write("TOML FILE CONTENT")
+        st.code(toml_content)
+        toml_file_name = json_file.name.replace(".json", ".toml")
+        st.download_button(
+            "DOWNLOAD TOML FILE", data=toml_content, file_name=toml_file_name
+        )
+        
 elif selected == "Options":
     st.title("Options")
+    
 elif selected == "JSON to TOML":
     st.title("JSON to TOML")
 
