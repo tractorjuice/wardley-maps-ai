14b38286f63f49d1416d78a72d8f5c2052569b97
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-09 10:45:23 +0100

    Update json2toml (beta).py

--- pages/json2toml (beta).py
+++ pages/json2toml (beta).py
@@ -102,9 +102,43 @@ elif selected == "WM to TOML":
             "DOWNLOAD TOML FILE", data=toml_content, file_name=toml_file_name
         )
         
-elif selected == "Options":
-    st.title("Options")
     
 elif selected == "JSON to TOML":
     st.title("JSON to TOML")
+    st.image("./pages/logo.gif", width=200)
+    st.title("JSON to TOML File Converter")
+    st.write(
+        """  
+            """
+    )
+    st.write(
+        """  
+    Let's convert your Wardley Map in JSON to TOML
+            """
+    )
+    
+    st.write(
+        """  
+            """
+    )
+    json_file = st.file_uploader("UPLOAD JSON FILE")
+    st.info(
+        f"""
+                👆 Upload your json file.
+                
+                """
+    )
 
+    if json_file is not None:
+        json_text = json_file.read()
+
+        st.write("JSON CONTENT")
+        st.code(json.loads(json_text))
+
+        toml_content = toml.dumps(json.loads(json_text))
+        st.write("TOML FILE CONTENT")
+        st.code(toml_content)
+        toml_file_name = json_file.name.replace(".json", ".toml")
+        st.download_button(
+            "DOWNLOAD TOML FILE", data=toml_content, file_name=toml_file_name
+        )
