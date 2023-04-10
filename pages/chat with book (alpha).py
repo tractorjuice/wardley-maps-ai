5d58c9bbe72be38174119296a44668004efac508
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-10 09:03:48 +0100

    Update chat with book (alpha).py

--- pages/chat with book (alpha).py
+++ pages/chat with book (alpha).py
@@ -57,7 +57,7 @@ chain = RetrievalQAWithSourcesChain.from_chain_type(
 
 query = "What is the history of Wardley Mapping?"
 result = chain(query)
-st.write("### Question:)
+st.write("### Question:")
 st.write(quey)
 st.write("### Answer:)
 st.write({result['answer']}    
