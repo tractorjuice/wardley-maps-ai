845f4f0e3619380400a06dbbf99d7ea1ef3237ba
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-10 09:07:13 +0100

    Update chat with book (alpha).py

--- pages/chat with book (alpha).py
+++ pages/chat with book (alpha).py
@@ -53,7 +53,7 @@ chain = RetrievalQAWithSourcesChain.from_chain_type(
 )
 
 #search_result = vector_store.similarity_search_with_score("What is the history of Wardley Maps?")
-#st.write(search_result)
+st.write(search_result)
 
 query = "What is the history of Wardley Mapping?"
 result = chain(query)
