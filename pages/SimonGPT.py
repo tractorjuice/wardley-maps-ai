0ab7bf58082e607fa6ee04cde653bab3c97013ee
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-15 08:31:57 +0100

    Update SimonGPT.py

--- pages/SimonGPT.py
+++ pages/SimonGPT.py
@@ -45,8 +45,6 @@ llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7, max_tokens=256)  #
 chain = RetrievalQAWithSourcesChain.from_chain_type(
     llm=llm,
     chain_type="stuff",
-    retriever=vector_store.as_retriever(),
-    return_source_documents=True,
     chain_type_kwargs=chain_type_kwargs
 )
 
