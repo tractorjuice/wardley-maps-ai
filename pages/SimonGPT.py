a66f995c91111fe779bd2454088ad809748457dc
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-15 08:59:28 +0100

    Update SimonGPT.py

--- pages/SimonGPT.py
+++ pages/SimonGPT.py
@@ -3,6 +3,7 @@ import streamlit as st
 from streamlit_chat import message
 from utils import get_initial_message, get_chatgpt_response, update_chat
 import os
+import openai
 from dotenv import load_dotenv
 load_dotenv()
 import openai
@@ -14,6 +15,27 @@ model = st.selectbox(
     "Select a model",
     ("gpt-3.5-turbo", "gpt-4")
 )
+
+def get_initial_message():
+    messages=[
+            {"role": "system", "content": "You are a helpful AI Tutor. Who anwers brief questions about AI."},
+            {"role": "user", "content": "I want to learn AI"},
+            {"role": "assistant", "content": "Thats awesome, what do you want to know aboout AI"}
+        ]
+    return messages
+
+def get_chatgpt_response(messages, model="gpt-3.5-turbo"):
+    print("model: ", model)
+    response = openai.ChatCompletion.create(
+    model=model,
+    messages=messages
+    )
+    return  response['choices'][0]['message']['content']
+
+def update_chat(messages, role, content):
+    messages.append({"role": role, "content": content})
+    return messages
+
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
