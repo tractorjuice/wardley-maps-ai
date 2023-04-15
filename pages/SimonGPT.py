3a6208270cd3ea12d4091f212013ee6d57f2c3b3
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-15 09:21:55 +0100

    Update SimonGPT.py

--- pages/SimonGPT.py
+++ pages/SimonGPT.py
@@ -63,6 +63,4 @@ if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
         message(st.session_state["generated"][i], key=str(i))
-
-    with st.expander("Show Messages"):
-        st.write(messages)
+        
